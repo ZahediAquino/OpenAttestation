c53fcfd57d8812a4b1a7edc5cad5f5feb3cb22c0
zaaquino <zahedi.aquino@intel.com>
2015-08-10 11:41:42 -0500

    Changing imports to: import com.intel.mtwilson.util.x509.X509Util;

--- services/AttestationService/src/main/java/com/intel/mtwilson/as/business/HostBO.java
+++ services/AttestationService/src/main/java/com/intel/mtwilson/as/business/HostBO.java
@@ -34,7 +34,7 @@ import com.intel.mtwilson.as.data.TblMle;
 import com.intel.mtwilson.as.data.TblTaLog;
 import com.intel.mtwilson.as.helper.BaseBO;
 import com.intel.mtwilson.crypto.CryptographyException;
-import com.intel.mtwilson.crypto.X509Util;
+import com.intel.mtwilson.util.x509.X509Util;
 import com.intel.mtwilson.datatypes.*;
 import com.intel.mtwilson.util.ResourceFinder;
 
