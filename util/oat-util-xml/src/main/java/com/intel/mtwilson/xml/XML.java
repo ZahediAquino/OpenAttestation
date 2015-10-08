5dd437387a5059b339e3434e4b4393c77932c8da
zaaquino <zahedi.aquino@intel.com>
2015-10-08 10:45:52 -0500

    Dashboard not loading server status

--- util/oat-util-xml/src/main/java/com/intel/mtwilson/xml/XML.java
+++ util/oat-util-xml/src/main/java/com/intel/mtwilson/xml/XML.java
@@ -71,8 +71,6 @@ public class XML {
 //            if(in == null) log.debug("parseDocumentElement - InputStream is NULL");
 //            else           log.debug("parseDocumentElement - InputStream OK");
             schemaSources[i] = new StreamSource(in);
-            if(in != null)
-                in.close();
             
             log.debug("parseDocumentElement - schemaSources[" + i + "] :" + schemaSources[i].getSystemId() );
             i++;
